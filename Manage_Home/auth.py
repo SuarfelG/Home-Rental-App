cea568319f0b0cbb5a618a83c86b1190f1ceb241
Surafel Gebrmedhin <surafelgmedhin7@gmail.com>
2024-06-27 18:10:20 +0300

    Basic Modification On User Authentication With Addition Of Security Mechanism

--- Manage_Home/auth.py
+++ Manage_Home/auth.py
@@ -4,9 +4,8 @@ from wtforms import StringField ,RadioField , SubmitField
 from wtforms.validators import DataRequired
 from . import db
 from .models import User
+from werkzeug.security import generate_password_hash , check_password_hash
 auth=Blueprint("auth", __name__)
-
-
 
 
 class RegstrationForm (FlaskForm):
@@ -14,26 +13,24 @@ class RegstrationForm (FlaskForm):
        middle_name=StringField('first_name' , validators=[DataRequired()])
        last_name=StringField('first_name' , validators=[DataRequired()])
        pasword=StringField('first_name' , validators=[DataRequired()])
-       AccountType=RadioField ("AccountType", choices=(["landlord",True] ,["renter",False] ))
+       AccountType=RadioField ("AccountType", choices=(["on",True] ,["off",False] ))
        Submit=SubmitField('signup')
 
 
 @auth.route("/login", methods=["POST","GET"])
 def login():
         if request.method=="POST":
-             id = request.form.get('id')
-             first_name=request.form.get('first_name')
-             middle_name=request.form.get('middle_name')
-             last_name=request.form.get('last_name')
              password=request.form.get('password')
              email=request.form.get('email')
-             AccountType=request.form.get('AccountType')
+             EmailExist=User.query.filter_by(email=email).first()
 
-             EmailExist=User.query.filter_by(email).first()
-             
-
-
-             flash("Logged In Successfuly", category="success")
+             if EmailExist:
+                    if check_password_hash(EmailExist.password,password):
+                            flash("Logged In Successfuly", category="success")
+                            return render_template("home.html" )
+                    else:
+                           flash("Unkown User", category="error")
+                           return render_template("signup.html")
              return render_template("signup.html")
                       
         return render_template("login.html")
@@ -41,22 +38,34 @@ def login():
 @auth.route("/signup", methods=["POST","GET"])
 def signup():
     if request.method=="POST":
-        Name=request.form.get("Name")
-        Father_Name=request.form.get("Middle")
-        Email=request.form.get("Email")
-        Password=request.form.get("Password")
-        user_type=request.form.get("user_type")
+        id = request.form.get('id')
+        first_name=request.form.get('Name')
+        middle_name=request.form.get('Middle')
+        last_name=request.form.get('Last')
+        email=request.form.get("Email")
+        password=request.form.get("password")
+        AccountType=request.form.get('flexRadioDefault')
+
+        EmailExist=User.query.filter_by(email=email).first()
+
         
-        if (len(Name)<3):
+        if EmailExist:
+                flash("Email Already Exist" , category="error")
+        elif (len(first_name)<3):
                 flash("Name is too short" , category="error")
-        elif (len(Father_Name)<3):
-                flash("Father_Name is too short" , category="error")
-        elif(len(Email)<4):
+        elif (len(middle_name)<3):
+                flash("Middle Name is too short" , category="error")
+        elif (len(last_name)<3):
+                flash("Last Name is too short" , category="error")
+        elif(len(email)<4):
                 flash("Email is too short" , category="error")
-        elif(len(Password))<4:
+        elif(len(password))<4:
                 flash("Password is too short" , category="error")
         else:
+                newuser=User(first_name=first_name , middle_name=middle_name , last_name=last_name ,email=email , password=generate_password_hash(password, method='pbkdf2:sha256')  , AccountType=AccountType )
+                db.session.add(newuser)
+                db.session.commit()
                 flash("Signed in Successfuly" , category="success")
-                return 'home.html'
+                return render_template("home.html")
         
     return render_template("signup.html")
